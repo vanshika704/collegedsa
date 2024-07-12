096b64e8f5296aa08ae15fd9346520a2e208387d
vanshika704 <sharmavanshi704@gmail.com>
2024-07-12 15:22:40 +0530

    input of number

--- array.cpp
+++ array.cpp
@@ -1,7 +1,29 @@
 #include <iostream>
 using namespace std;
+
 int main()
 {
     int array[9][9];
-    cout << "Elements of array";
+
+    cout << "Enter elements of the array:" << endl;
+
+    for (int i = 0; i < 9; ++i)
+    {
+        for (int j = 0; j < 9; ++j)
+        {
+            cin >> array[i][j];
+        }
+    }
+
+    cout << "Elements of the array:" << endl;
+    for (int i = 0; i < 9; ++i)
+    {
+        for (int j = 0; j < 9; ++j)
+        {
+            cout << array[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    return 0;
 }

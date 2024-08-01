3503fbf735013b916703a6d724c07715a3cf2099
vanshika704 <sharmavanshi704@gmail.com>
2024-08-02 00:17:17 +0530

    .

--- Sorting/teenosort.cpp
+++ Sorting/teenosort.cpp
@@ -3,14 +3,14 @@
 using namespace std;
 void selection_sort(int arr[], int n) {
   // selection sort
-  for (int i = 0; i < n - 1; i++) {
-    int mini = i;
-    for (int j = i + 1; j < n; j++) {
-      if (arr[j] < arr[mini]) {
+  for (int i = 0; i < n - 1; i++) {// i  zero se shuru hoga or n-1 tak chlega 
+    int mini = i; // initialise kiya mini , value shuru ki  i se
+    for (int j = i + 1; j < n; j++) { // j initialise kiya , i+1 se chlega n tk , 
+      if (arr[j] < arr[mini]) { // agar humara present index , mini se chota h , toh present index ko mini bnado
         mini = j;
       }
     }
-    int temp = arr[mini];
+    int temp = arr[mini]; // temp variable ke trough swapping ki h
     arr[mini] = arr[i];
     arr[i] = temp;
   }
@@ -22,6 +22,25 @@ void selection_sort(int arr[], int n) {
   cout << "\n";
 
 }
+void bubble_sort(int arr[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = 0; j < n - i - 1; j++) {
+            if (arr[j] > arr[j + 1]) {
+                // Swap arr[j] and arr[j + 1]
+                int temp = arr[j];
+                arr[j] = arr[j + 1];
+                arr[j + 1] = temp;
+            }
+        }
+    }
+    cout << "After Bubble sort: " << "\n";
+  for (int i = 0; i < n; i++) {
+    cout << arr[i] << " ";
+  }
+  cout << "\n";
+
+
+}
 int main() {
   int arr[] = {13,46,24,52,20,9};
   int n = sizeof(arr) / sizeof(arr[0]);
@@ -31,5 +50,6 @@ int main() {
   }
   cout << "\n";
   selection_sort(arr, n);
+  bubble_sort(arr, n);
   return 0;
 }
